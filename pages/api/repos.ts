900eea46f72076d19b9ff2b6330d233b2a490eb3
Marek <marek.tancak16@gmail.com>
2022-11-22 20:45:51 +0000

    Add an api endpoint to fetch public repositories

---
+++ pages/api/repos.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let userData = await fetch('https://api.github.com/users/' + process.env.GH_USERNAME + "/repos")
+    .then(response => {
+      if (!response.ok) {
+        res.status(500);
+      }
+      return response.json()
+    })
+  
+  res.status(200).json(userData);
+}
