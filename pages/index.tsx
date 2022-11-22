2a8e3c3eacf28bffa98850c7d056dbc44364bd2d
Marek <marek.tancak16@gmail.com>
2022-11-22 21:35:20 +0000

    Fetch the projects from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
 import { Bio } from "../components/bio"
 import { Gallery } from "../components/gallery";
+import { Profile, Repo } from "../utils/profile";
 
-type Profile = {
-  name: string;
-  avatar_url: string;
-  bio: string;
+type Data = {
+  profile: Profile;
+  repos: Repo[]
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(process.env.SITE_URL + '/api/user/' + process.env.GH_USERNAME);
-  const profile = await res.json();
-  return { props: profile };
+  const resProfile = fetch(process.env.SITE_URL + '/api/user/' + process.env.GH_USERNAME);
+  const resRepos = fetch(process.env.SITE_URL + '/api/repos/' + process.env.GH_USERNAME);
+  
+  const profile = (await resProfile).json();
+  const repos = (await resRepos).json();
+
+  return { props: { profile: await profile, repos: await repos} };
 }
 
-function Home(profile: Profile) {
+function Home(data: Data) {
+  console.log(data)
   return (
     <div className="flex flex-col m-10">
-      <Bio name={ profile.name } avatar_url={ profile.avatar_url } bio={ profile.bio }/>
-      <Gallery/>
+      <Bio name={ data.profile.name } avatar_url={ data.profile.avatar_url } bio={ data.profile.bio }/>
+      <Gallery repos={ data.repos } />
     </div>
   );
 }
---
+++ utils/profile.ts
@@ -0,0 +1,11 @@
+export type Profile = {
+  name: string;
+  avatar_url: string;
+  bio: string;
+}
+
+export type Repo = {
+  name: string;
+  description: string;
+  html_url: string;
+}
